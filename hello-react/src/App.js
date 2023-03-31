8e87f95dded5ed8be05365a5eea5dd3e52e7eac0
IamAnkitR <ardb8755@gmail.com>
2023-03-31 07:46:18 +0530

    Task added

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import TaskCard from "./TaskCard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div class="grid grid-cols-6">
+      <div class="col-start-2 col-span-4">
+        <p className="main">
+          <strong>Smarter Tasks</strong>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className="p">
+          <strong>Project:</strong> Graduation Final Year Project (Tula's Institue Dehradun)
+        </p>
+        <div className="container">
+          <h1 className="h1">
+            <strong>Pending</strong>
+          </h1>
+          <TaskCard
+            title="Build the website"
+            dueDate="10th April"
+            assigneeName="Ankit R"
+          />
+          <TaskCard
+            title="Add a blog"
+            dueDate="22nd  March"
+            assigneeName="Ankit R"
+          />
+          <div className="new">
+          <a  href="/">+ New Task </a>
+          </div>
+        </div>
+        <div className="container">
+          <h1 className="h1">
+            <strong>Done</strong>
+          </h1>
+          <TaskCard
+            title="Design the mockup"
+            completedAtDate="10th April"
+            assigneeName="Ankit R"
+          />
+          <TaskCard
+            title="Get approval from principal"
+            completedAtDate="20th April"
+            assigneeName="Ajay S"
+          />
+          <br></br>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
