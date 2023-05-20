e4ed5c68b31f87ce75503255fd9a28ac296cbca5
IamAnkitR <ardb8755@gmail.com>
2023-05-20 21:36:04 +0530

    routes add

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,10 +1,15 @@
 import TaskApp from "./TaskApp";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./HomePage";
+import TaskDetailsPage from "./TaskDetailspage";
 
 function App() {
   return (
-    <div className="App">
-      <TaskApp />
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/tasks" element={<TaskApp />} />
+      <Route path="/tasks/:id" element={<TaskDetailsPage />} />
+    </Routes>
   );
 }
 
