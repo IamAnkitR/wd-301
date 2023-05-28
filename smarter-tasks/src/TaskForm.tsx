7541f1d659232881f2a962a170d4ab142af4654a
IamAnkitR <ardb8755@gmail.com>
2023-05-28 20:03:32 +0530

    input required condition

--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -38,11 +38,12 @@ const TaskForm = (props: TaskFormProps) => {
   };
   const addTask: React.FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
-    console.log(`Submitted the form with`);
-    console.log(formState);
     if (formState.title.length === 0 || formState.dueDate.length === 0) {
+      alert("Add a title and due-date");
       return;
     }
+    console.log(`Submitted the form with`);
+    console.log(formState);
     props.addTask(formState);
     setFormState({
       id: uid,
@@ -63,7 +64,6 @@ const TaskForm = (props: TaskFormProps) => {
             onChange={titleChanged}
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
-            required
           />
           <label
             htmlFor="todoTitle"
@@ -81,7 +81,6 @@ const TaskForm = (props: TaskFormProps) => {
             onChange={descriptionChanged}
             placeholder=" "
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            required
           />
           <label
             htmlFor="todoDescription"
@@ -99,7 +98,6 @@ const TaskForm = (props: TaskFormProps) => {
             onChange={dueDateChanged}
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
-            required
           />
           <label
             htmlFor="todoDueDate"
