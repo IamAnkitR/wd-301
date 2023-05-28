2889e8f7f15dce22aa26e00a75c216f84b4f8b6d
IamAnkitR <ardb8755@gmail.com>
2023-05-28 20:46:53 +0530

    debug

--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -54,7 +54,7 @@ const TaskForm = (props: TaskFormProps) => {
   };
   return (
     <form onSubmit={addTask}>
-      <div className="TaskItem grid md:grid-cols-4 md:gap-3 ">
+      <div className="grid md:grid-cols-4 md:gap-3">
         <div className="relative z-0 w-full mb-6 group">
           <input
             id="todoTitle"
@@ -64,7 +64,6 @@ const TaskForm = (props: TaskFormProps) => {
             onChange={titleChanged}
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
-            required
           />
           <label
             htmlFor="todoTitle"
@@ -112,7 +111,6 @@ const TaskForm = (props: TaskFormProps) => {
           <button
             type="submit"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            id="addTaskButton"
           >
             Add item
           </button>
